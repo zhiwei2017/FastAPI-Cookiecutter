a6ddd0bb71eca8d6c1c3ccad5473bb8affa796c3
Zhiwei Zhang <zhiwei2017@gmail.com>
2023-06-05 10:51:22 +0200

    Bump version: 0.1.0 → 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f'{datetime.datetime.now().year}, {author}'
 
 
 # The short X.Y version
-version = '0.1.0'
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
